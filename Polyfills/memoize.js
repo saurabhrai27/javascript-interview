ef808be34b64ffedc2dd7d791cf0f6ce99ee4dfa
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-04-22 12:52:49 +0530

    polyfill of memoize function

---
+++ Polyfills/memoize.js
@@ -0,0 +1,19 @@
+
+function memoize (fn) {
+    const cache = new Map();
+    return (...args) => {
+        const argsToStrings = JSON.stringify(args);
+        if(cache.has(argsToStrings)) {
+            return cache.get(argsToStrings)
+        } else {
+            const result = fn.apply(this, args)
+            cache.set(argsToStrings, result);
+            return result;
+        }
+    }
+}
+
+const addThreeNums = (a,b,c) => a+b+c
+const add = memoize(addThreeNums)
+console.log(add(1,2,3));
+console.log(add(1,2,3));
